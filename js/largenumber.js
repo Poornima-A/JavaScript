5d6d27cbdeea1fbce2f22e0f657b10440319dfe4
Poornima-A <poornimaarumugam2696@gmail.com>
2024-12-13 19:07:16 +0530

    Add files via upload

---
+++ js/largenumber.js
@@ -0,0 +1,8 @@
+function findLargest(num1, num2, num3) {
+    return Math.max(num1, num2, num3);
+    }
+    
+    // Example usage:
+    const largestNumber = findLargest(10, 5, 8);
+    console.log("Largest number:", largestNumber);
+    
